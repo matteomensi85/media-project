a6b4f04510cc39ba061b70367cfa335eecfcb507
matteomensi85 <matteo.mensi@katamail.com>
2022-02-25 15:17:17 +0100

    vimeo api update

--- src/app/vimeo-api.service.ts
+++ src/app/vimeo-api.service.ts
@@ -1,20 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { apiAccess } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VimeoApiService {
-
+  private token: string = apiAccess.vimeoAccessKey;
+  private headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
   constructor(private http: HttpClient) { }
 
   getVimeo(query: string) {
     
     
-
     let apiUrl: string = `https://api.vimeo.com/videos?query=${query}`;
     console.log(apiUrl);
-    return this.http.get(apiUrl);
+    return this.http.get(apiUrl, {'headers': this.headers});
 
   }
 }
